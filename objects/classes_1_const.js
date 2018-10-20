c77641605077cbf280446ebbdd3d5a9a9c8804cf
kost.nik88@gmail.com <hozonti99>
2018-10-20 14:23:33 +0300

    objects, classes

--- objects/classes_1_const.js
+++ objects/classes_1_const.js
@@ -1,12 +1,27 @@
 class User {
-    constructor(name) {
+
+    constructor(name, surname) {
+        this.surname = surname;
         this.name = name;
     }
 
+    set fullName(value) {
+        [this.name, this.surname] = value.split(' ');
+    }
+
+    static getNew() {
+        return new User("test", "user");
+    }
+
     sayHi() {
-        console.log(this.name);
+        console.log(this.name, "->", this.surname);
     }
 }
 
-let user = new User("NAME");
+let user = new User("NAME surnamE");
 user.sayHi();
+user.fullName = "Name splitted";
+user.sayHi();
+
+let new1 = User.getNew();
+new1.sayHi();
